2a8a90bd4c236714b31d98899257950236b69653
Terra Ali Fenton <tfenton@csumb.edu>
2017-03-28 17:14:25 +0000

    Added second half of cart file

--- cart.php
+++ cart.php
@@ -63,3 +63,52 @@ else
                         $val = $val+1;
                 }
     }
+    else
+    {
+        $_SESSION['cart'] = $_SESSION['cart'];
+        $wherein = implode(',',$_SESSION['cart']);
+         echo"
+                <body>
+                <table class = 'table-fill'>
+                <thead>
+                    <tr>
+                        <th>Product Name</th>
+                        <th>Product Price</th>
+             
+                    </tr>
+                </thead>
+                <tbody>";
+                $servername = getenv('IP');
+                $dbPort = 3306;
+                $database = "Overwatch";
+                $username = getenv('C9_USER');
+                $password = "";
+                $dbConn = new PDO("mysql:host=$servername;port=$dbPort;dbname=$database", $username, $password);
+                $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $sql ="SELECT Product.*
+                        FROM Product
+                        WHERE Product.productId IN ($wherein)
+                        ";
+                $stmt = $dbConn->prepare($sql);
+                $stmt->execute ();
+                $val = 0;
+                while ($row = $stmt->fetch())  
+                {
+                        echo "
+                        <tr>
+                            <td>
+                                <div class = \"popup\" onclick = \"myFunction($val)\">
+                                 ".$row['productName']."
+                                <span class=\"popuptext\" id=$val>".$row['desc']."</span>
+                                </div>
+                            </td>
+                                <td>"."$".$row['price']."</td>
+                        </tr>";
+                        $val = $val+1;
+                }
+    }
+}
+?>
+</body>
+</html>
+<a class = "button"href = "main.php">Go Back To Main Page</a>
