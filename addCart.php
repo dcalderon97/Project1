ff5b1725ba56a9f615a1b641bf32803d9b611883
Terra Ali Fenton <tfenton@csumb.edu>
2017-03-28 17:16:53 +0000

    Added html for addCart file. Project Done.

--- addCart.php
+++ addCart.php
@@ -6,3 +6,15 @@ if(empty($_SESSION['cart'])){
 }
 array_push($_SESSION['cart'],$_GET['id']);
 ?>
+<html>
+  <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+  </head>  
+  <body>
+      <h1 class = "raleWay"align = "center">Product Was Added To Cart</h1>
+      <div align = "center">
+        <a class = "button"style = "text-align:center "href = "cart.php?go=true">View Shopping Cart</a>
+        <a class = "button"href = "main.php">Go Back To Main Page</a>
+      </div>
+  </body>
+</html>
